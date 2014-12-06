62ded31e91cca2e849d9f64e033c274172f867e1
Christopher D Canfield <cdcanfield@gmail.com>
2014-12-06 10:44:23 -0500

    Began implementing GameApp class; Updated gitignore

--- ludum-dare-31/core/src/com/divergentthoughtsgames/ld31/GameApp.java
+++ ludum-dare-31/core/src/com/divergentthoughtsgames/ld31/GameApp.java
@@ -2,24 +2,38 @@ package com.divergentthoughtsgames.ld31;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class GameApp extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private FPSLogger fpsLogger;
+	
+	private SpriteBatch batch;
+	private Texture img;
+	
+	private OrthographicCamera camera;
+	
+	private World world;
 	
 	@Override
 	public void create () {
+		fpsLogger = new FPSLogger();
+		
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+//		world = new World();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
